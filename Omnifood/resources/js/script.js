318baaf1047bb7ddc6826a9bb1b850f24eeb3677
obiwan31415 <obiwan314@gmail.com>
2018-03-27 22:57:18 +0200

    Add animations triggered on scroll

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -64,5 +64,26 @@ $(document).ready(function () {
       });
     /* END of Navigation scroll*/
     
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
     
-});
+}); // $(document).ready(function ()
